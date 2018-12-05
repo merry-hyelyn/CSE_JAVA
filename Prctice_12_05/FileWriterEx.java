91f3b982187b6cfe7920c187f1e7f88cf6070814
merry-Hyelyn <h1335@naver.com>
2018-12-05 23:14:48 +0900

    Update

---
+++ Prctice_12_05/FileWriterEx.java
@@ -0,0 +1,27 @@
+﻿import java.io.*;
+import java.util.*;
+
+public class FileWriterEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s =new Scanner(System.in);
+		FileWriter fout = null;
+		int c;
+		try {
+			fout = new FileWriter("c:\\temp\\test.txt");
+			while(true) {
+				String line = s.nextLine();
+				if(line.length()==0)
+					break;
+				fout.write(line,0,line.length());
+				fout.write("\r\n",0,2);
+				System.out.println("c:\\temp\\test.txt 에 저장하였습니다.");
+			}
+			fout.close();
+		}catch(IOException e) {
+			System.out.println("입출력 오류");
+		}
+		s.close();
+	}
+}
