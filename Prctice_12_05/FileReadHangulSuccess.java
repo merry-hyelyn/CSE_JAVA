c2ce70ec3a4e0fa1757e25f514714affb69729d2
merry-Hyelyn <h1335@naver.com>
2018-12-05 23:14:21 +0900

    Update

---
+++ Prctice_12_05/FileReadHangulSuccess.java
@@ -0,0 +1,24 @@
+﻿import java.io.*;
+
+public class FileReadHangulSuccess {
+
+	public static void main(String[] args) {
+		InputStreamReader in = null;
+		FileInputStream fin = null;
+		try {
+			fin = new FileInputStream("c:\\temp\\hangul.txt");
+			// MS949 = 한글을 사용하겠다는 문자 집합
+			in = new InputStreamReader(fin, "MS949");
+			int c;
+		
+		System.out.println("인코딩 문자 집합은 "+in.getEncoding());
+		while((c=in.read())!=-1) {
+			System.out.print((char)c);
+		}
+		in.close();
+		fin.close();
+	}catch(IOException e) {
+		System.out.println("입출력 오류");
+	}
+	}
+}
