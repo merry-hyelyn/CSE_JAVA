6e911063cb7f049e5d3b1b26a9238d86bedba6b5
merry-Hyelyn <h1335@naver.com>
2018-12-05 23:15:28 +0900

    Practice JAVA

---
+++ Prctice_12_05/Practice_08_01.java
@@ -0,0 +1,27 @@
+﻿import java.io.*;
+import java.util.Scanner;
+
+public class Practice_08_01 {
+
+	public static void main(String[] args) {
+		System.out.println("전화번호 입력 프로그램입니다.");
+		Scanner s = new Scanner(System.in);
+		FileWriter fout = null;
+		try {
+			fout = new FileWriter("c:\\temp\\phone.txt");
+			while(true) {
+				System.out.print("이름 전화번호 >> ");
+				String line = s.nextLine();
+				if(line.equals("그만"))
+					break;
+				fout.write(line,0,line.length());
+				fout.write("\r\n",0,2);
+			}
+			fout.close();
+		}catch(IOException e) {
+			System.out.println("입출력 오류");
+		}
+		s.close();
+	}
+
+}
---
+++ Prctice_12_05/Practice_08_03.java
@@ -0,0 +1,19 @@
+﻿import java.io.*;
+
+public class Practice_08_03 {
+
+	public static void main(String[] args) {
+		FileReader fi = null;
+		try {
+			fi = new FileReader("c:\\windows\\system.ini");
+			int c;
+			while((c = fi.read())!=-1) {
+				c = Character.toUpperCase(c);
+				System.out.print((char)c);
+			}
+			fi.close();
+		}catch(IOException e ) {
+			System.out.println("입출력 오류");
+		}
+	}
+}
